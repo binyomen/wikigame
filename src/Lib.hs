dc13d680b461c58b7795a2ea5e92e6a7681df195
Ben Weedon <ben@weedon.email>
2020-02-08 19:08:30 -0800

    Print out titles from wikipedia pages

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
     ( playGame
     ) where
 
+import Text.HTML.Scalpel (URL, scrapeURL, text, (@:), (@=))
+
+couldNotReadPageTitleError :: String
+couldNotReadPageTitleError = "Could not read page title"
+
 playGame :: String -> String -> IO ()
-playGame startUrl endUrl = putStrLn $ startUrl ++ " " ++ endUrl
+playGame startUrl endUrl = do
+    startTitle <- getTitle startUrl >>= convertMaybe couldNotReadPageTitleError
+    endTitle <- getTitle endUrl >>= convertMaybe couldNotReadPageTitleError
+    putStrLn $ startTitle ++ ", " ++ endTitle
+
+getTitle :: URL -> IO (Maybe String)
+getTitle url =
+    scrapeURL url scraper
+    where
+        scraper = text $ "h1" @: ["id" @= "firstHeading"]
+
+convertMaybe :: String -> Maybe a -> IO a
+convertMaybe _ (Just v) = return v
+convertMaybe errMsg Nothing = fail errMsg
