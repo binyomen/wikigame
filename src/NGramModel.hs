292b7233801467654ddd089eb7376acc87346448
Ben Weedon <ben@weedon.email>
2020-02-14 21:06:59 -0800

    Add tokenizer

--- src/NGramModel.hs
+++ src/NGramModel.hs
@@ -3,6 +3,7 @@ module NGramModel
     , makeModel
     ) where
 
+import Data.Char (isSpace)
 import Data.Map.Strict (Map)
 
 data WordMap =
@@ -16,8 +17,37 @@ data NGramModel = NGramModel
 
 makeModel :: Int -> String -> NGramModel
 makeModel n text =
-    NGramModel { ngm_n = n, ngm_data = parseText text }
+    NGramModel { ngm_n = n, ngm_data = parse text }
 
-parseText :: String -> WordMap
-parseText text =
+tokenize :: String -> [String]
+tokenize text =
+    word : tokenize rest
+    where
+        (word, rest) = getWord text
+
+getWord :: String -> (String, String)
+getWord (first : rest) =
+    if isSpace first then
+        ("", eatSpaces rest)
+    else
+        (first : restWord, restWords)
+    where
+        (restWord, restWords) = getWord rest
+getWord [] =
+    ("", [])
+
+eatSpaces :: String -> String
+eatSpaces (first : rest) =
+    if isSpace first then
+        eatSpaces rest
+    else
+        first : rest
+eatSpaces [] = ""
+
+constructMap :: [String] -> WordMap
+constructMap words =
     Word "test"
+
+parse :: String -> WordMap
+parse =
+    constructMap . tokenize
