84a2b4f5ee68d8858426431599c6e68818dca571
Ben Weedon <ben@weedon.email>
2020-02-23 16:33:26 -0800

    Count the number of hops to complete

--- src/Lib.hs
+++ src/Lib.hs
@@ -14,19 +14,19 @@ playGame :: URL -> URL -> IO ()
 playGame startUrl endUrl = do
     hSetEncoding stdout utf8
     crawler <- makeCrawler startUrl endUrl :: IO NGramCrawler
-    gameLoop crawler startUrl endUrl
+    gameLoop crawler startUrl endUrl 0
 
-gameLoop :: Crawler a => a -> URL -> URL -> IO ()
-gameLoop crawler currentUrl endUrl =
+gameLoop :: Crawler a => a -> URL -> URL -> Word -> IO ()
+gameLoop crawler currentUrl endUrl pagesVisited =
     if currentUrl == endUrl then
-        putStrLn "\nFinished!!!"
+        putStrLn $ "\nFinished in " ++ show (pagesVisited - 1) ++ " hops!!!"
     else do
         (newCrawler, newPage) <- nextPage crawler
         let sourceLinkText = getSourceLinkText (p_sourceLinkText newPage) ++ " -> "
         let title = p_title newPage ++ " | "
         let url = p_url newPage
         putStrLn $ addSpaces sourceLinkText ++ addSpaces title ++ url
-        gameLoop newCrawler (p_url newPage) endUrl
+        gameLoop newCrawler (p_url newPage) endUrl $ pagesVisited + 1
 
 getSourceLinkText :: Maybe String -> String
 getSourceLinkText = fromMaybe "<START_PAGE>"
